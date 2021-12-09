865177a4883acdfb036ace6b9e973792584121b2
Mindii Emanuel-Romica <emanuel.mindii@emmanuel.ro>
2021-12-09 16:43:22 +0200

    Hi

--- OOP1/OOP1/ArrayHelper.cs
+++ OOP1/OOP1/ArrayHelper.cs
@@ -6,8 +6,31 @@ using System.Threading.Tasks;
 
 namespace OOP1
 {
-    class ArrayHelper
+    public class ArrayHelper
     {
-
+        public static int[] Fibonacci(int n)
+        {
+            int[] F = new int[n];
+            F[0] = 0;
+            if (n == 0) return F;
+            F[1] = 1;
+            if (n == 1) return F;
+            int i = 1;
+            while (i < n-1)
+            {
+                i++;
+                F[i] = F[i - 1] + F[i - 2];
+            }
+            return F;
+        }
+        public static int Frecventa(int[] vector, int element)
+        {
+            int frecventa = 0;
+            for (int i = 0; i <= vector.Length; i++)
+            {
+                if (vector[i] == element) frecventa++;
+            }
+            return frecventa;
+        }
     }
 }
--- OOP1/OOP1/Program.cs
+++ OOP1/OOP1/Program.cs
@@ -4,8 +4,34 @@ namespace OOP1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+           int n;
+            n = Int32.Parse(Console.ReadLine());
+           int[] vector = ArrayHelper.Fibonacci(n);
+            for (int i = 0; i < vector.Length; i++)
+                Console.WriteLine(vector[i] + " ");
+            n = Int32.Parse(Console.ReadLine());
+            int[] v = new int[n];
+
+            int nr = Int32.Parse(Console.ReadLine());
+            int[] vector1 = new int[nr];
+
+            for (int i = 0; i < nr; i++)
+            {
+                Console.WriteLine($"vector1[{i}]");
+                vector1[i] = Int32.Parse(Console.ReadLine());
+
+            }
+            int element = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(ArrayHelper.Frecventa(vector1, element));
+
+
+            Console.WriteLine("Introduceti numarul de linii!");
+
+            int n1 = Int32.Parse(Console.ReadLine());
+            int[,] matrix = new int[n1, n1];
         }
     }
 }
